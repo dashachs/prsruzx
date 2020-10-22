00342d7ccad75483dff8445b86c7aaea51407ff5
Anwar Anarkulov <anwar.anarkulov.2302@gmail.com>
2020-10-22 16:05:28 +0500

    start

---
+++ parser.py
@@ -0,0 +1,8 @@
+from bs4 import BeautifulSoup
+import requests as req
+
+# resp = req.get('http://railway.uz/ru/informatsionnaya_sluzhba/tendery/mejdunorodniy/20079/')
+
+# soup = BeautifulSoup(resp.text, 'lxml')
+
+print("hello world")
