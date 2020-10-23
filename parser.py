1b3c853c46f74a4a90f945b496d567eb33dc614c
Anwar Anarkulov <anwar.anarkulov.2302@gmail.com>
2020-10-23 22:20:05 +0500

    installed lxml, selenium test parser (don`t work with <app-root>)

--- parser.py
+++ parser.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 import requests as req
 
-# resp = req.get('http://railway.uz/ru/informatsionnaya_sluzhba/tendery/mejdunorodniy/20079/')
+resp = req.get('http://etender.uzex.uz/lots/1/0')
 
-# soup = BeautifulSoup(resp.text, 'lxml')
+soup = BeautifulSoup(resp.text, 'lxml')
 
-print("hello world")
+
+app = soup.find('app-root')
+
+print(app, type(app), sep='\n')
