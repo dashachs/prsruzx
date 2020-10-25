2da02e0c20e7d4ddbd724f2e0e88620705612a8a
Anwar Anarkulov <anwar.anarkulov.2302@gmail.com>
2020-10-25 21:38:22 +0500

    цикл исправлен (обращение теперь по индексу) браузер работает без головы

--- parser.py
+++ parser.py
@@ -6,6 +6,9 @@ options.add_argument('--headless')
 # start chrome browser
 browser = webdriver.Chrome('C:\\IT\\Python\\Projects\\uzexparser\\chromedriver.exe', options=options)
 browser.get('http://etender.uzex.uz/lots/1/0')
-res = browser.find_element_by_class_name('lot-item')
-print(browser.page_source)
+res = browser.find_elements_by_class_name('lot-item')
+for i in range(len(res)):
+    print(i)
+    print(res[i].text)
+    print('\n\n')
 browser.quit()
