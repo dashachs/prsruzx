204b68fbab292b465afe36ac3db7c42d11fe5c9f
Anwar Anarkulov <anwar.anarkulov.2302@gmail.com>
2020-10-26 11:20:09 +0500

    кнопка работет:)

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver import ActionChains
 
 options = webdriver.ChromeOptions()
@@ -10,12 +11,15 @@ browser.get('http://etender.uzex.uz/lots/1/0')
 
 # print(browser.page_source)
 
-# buttons = browser.find_elements_by_css_selector('button.btn-outline-primary')
-# for i in buttons:
-#     print(i, type(i))
-#
-# for i in range(100):
-#     buttons[1].click()
+buttons = browser.find_elements_by_css_selector('button.btn-outline-primary')
+for i in buttons:
+    print(i, type(i))
+
+try:
+    while True:
+        buttons[1].click()
+except StaleElementReferenceException:
+    print('кнопки больше нет')
 
 res = browser.find_elements_by_class_name('lot-item')
 for i in range(len(res)):
