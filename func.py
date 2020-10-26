4439c871a44c17c4fb6a295b5397b042a1af98de
chvanova.d <chvanovadasha@gmail.com>
2020-10-26 14:24:15 +0500

    202010261423 (уже что-то со страницы лотов)

--- func.py
+++ func.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.common.exceptions import StaleElementReferenceException
 
-
 def find_loadButton(browser):
     buttons = browser.find_elements_by_css_selector('button.btn-outline-primary')
     for i in buttons:
@@ -15,3 +14,11 @@ def press_loadButton(button):
             button.click()
     except StaleElementReferenceException:
         pass
+
+
+def parseLot(browser, link):
+    browser.get(link)
+    print(" parseLot")
+    res = browser.find_elements_by_xpath("//div[@class ='mb-4']/div/div/p")
+    for i in res:
+        print(i.text)
