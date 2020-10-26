c53e4eb962694e3205218c73658df40e6c653b75
Anwar Anarkulov <anwar.anarkulov.2302@gmail.com>
2020-10-26 11:14:49 +0500

    путь исправлен

--- parser.py
+++ parser.py
@@ -5,17 +5,17 @@ options = webdriver.ChromeOptions()
 options.add_argument('--headless')
 
 # start chrome browser
-browser = webdriver.Chrome('C:\\IT\\Python\\Projects\\uzexparser\\chromedriver.exe', options=options)
+browser = webdriver.Chrome('chromedriver.exe', options=options)
 browser.get('http://etender.uzex.uz/lots/1/0')
 
 # print(browser.page_source)
 
-buttons = browser.find_elements_by_css_selector('button.btn-outline-primary')
-for i in buttons:
-    print(i, type(i))
-
-for i in range(100):
-    buttons[1].click()
+# buttons = browser.find_elements_by_css_selector('button.btn-outline-primary')
+# for i in buttons:
+#     print(i, type(i))
+#
+# for i in range(100):
+#     buttons[1].click()
 
 res = browser.find_elements_by_class_name('lot-item')
 for i in range(len(res)):
