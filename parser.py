048998711025b83a5f0aeeccffbe213bebff5f71
chvanova.d <chvanovadasha@gmail.com>
2020-10-28 01:27:51 +0500

    202010280127 * парсинг * заполнены (кажется):     все, кроме attachedFile (там кнопка)

--- parser.py
+++ parser.py
@@ -16,15 +16,19 @@ func.press_loadButton(loadButton)
 # search lot's ID and purchase names
 lotIDs = []
 lotNames = []
+lotAdresses = []
 listForIDs = browser.find_elements_by_xpath("//div[@class ='lot-item__num-cat']/div/span")
 listForNames = browser.find_elements_by_xpath("//div[@class='lot-item__title']")
-for i, j in zip(listForIDs, listForNames):
+listOfAdresses = browser.find_elements_by_xpath("//div[@class='lot-item__address']")
+for i, j, k in zip(listForIDs, listForNames, listOfAdresses):
     lotIDs.append(i.text)
     lotNames.append(j.text)
+    lotAdresses.append(k.text)
 
 # clear lists
 listForIDs.clear()
 listForNames.clear()
+listOfAdresses.clear()
 
 # create lot's object
 listOfLots = []
@@ -33,7 +37,7 @@ listOfLots = []
 for i in range(len(lotNames)):
     link = "http://etender.uzex.uz/lot/" + lotIDs[i]
     # adding new lot to list of lots (adding ID and purchase name)
-    listOfLots.append(object_of_lot.lot(lotIDs[i], 1, 1, 1, 1, lotNames[i], 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1))
+    listOfLots.append(object_of_lot.lot(lotIDs[i], 1, 1, 1, 1, lotNames[i], 1, 1, 1, lotAdresses[i], 1, 1, 1, 1, 1, 1, 1))
     print(listOfLots[i].lotID, ": ", link)
     print(listOfLots[i].purchaseName)
     func.parseLot(browser, link, listOfLots[i])
