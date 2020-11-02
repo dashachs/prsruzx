ec0e3e8124324bf71ecf5eda83dda4de9e889724
Anwar Anarkulov <anwar.anarkulov.2302@gmail.com>
2020-11-02 18:15:57 +0500

    added dbUser.py
    функция для получения id категории

---
+++ dbUser.py
@@ -0,0 +1,50 @@
+import psycopg2
+
+con = psycopg2.connect(
+        database="postgres",
+        user="anwar",
+        password="etender.uz",
+        host="database-rds.cbs8omqsohea.eu-west-3.rds.amazonaws.com",
+        port="5432"
+    )
+print("Database opened successfully")
+
+# def inputToDB(con):
+#     cur = con.cursor()
+#     cur.execute(
+#         "INSERT INTO etender_uzex_test (lot_number, description, name, TIN, start_date, end_date, telephone, email, file_link)"
+#         "VALUES({}, '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}')".format(a, b, c, d, e, f, g, h, i))
+#     print("Date inserted successfully")
+#     con.commit()
+#     con.close()
+#
+#
+# def deleteRow(id, con):
+#     cur = con.cursor()
+#     cur.execute("DELETE FROM etender_uzex_test WHERE lot_number={}".format(id))
+#     print("Deletion successful")
+#     con.commit()
+#     con.close()
+#
+#
+# def amountOfRow(con):
+#     cur = con.cursor()
+#     cur.execute("SELECT count(lot_number) FROM etender_uzex_test")
+#     res = cur.fetchall()
+#     con.commit()
+#     con.close()
+#     return res[0][0]
+
+def getCategoryId(con, requierd):
+    cur = con.cursor()
+    cur.execute("SELECT category_id, name FROM bidding_categories_translations")
+    rows = cur.fetchall()
+    for row in rows:
+        if row[1].lower().replace(' ', '') == requierd.lower().replace(' ', ''):
+            return row[0]
+    print("getCategoryId done successfully")
+
+
+res = getCategoryId(con, 'ЮридичеСкИе   услу г  и')
+print(res)
+con.close()
